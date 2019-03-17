f87b4048bd5e90b6101e69b6a2f28fa1d286a43f
wa1tnr <wa1tnr@users.noreply.github.com>
2019-03-17 04:22:38 +0000

    On branch br-aaa NEW FILE 	new file:   local_source.fs

---
+++ arm/storage/emulated/0/abcdefg.d/forth.d/local_source.fs
@@ -0,0 +1,14 @@
+\  file: /storage/emulated/0/abcdefg.d/forth.d/local_source.fs
+
+\  date: 17 March 2019
+
+0 [IF]
+
+Any text you like, here.
+
+[THEN]
+
+." Top of Display - keywords, notes &c." cr cr
+
+." Do not use a path to include this file. " cr
+." I am /storage/emulated/0/abcdefg.d/forth.d/local_source.fs " cr cr
