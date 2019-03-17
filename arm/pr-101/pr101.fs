b5ce53753d3e659997d06f50e2c4110fda7a9920
wa1tnr <wa1tnr@users.noreply.github.com>
2019-03-17 01:45:01 +0000

    On branch br-aaa 	modified:   pr101.fs

--- arm/pr-101/pr101.fs
+++ arm/pr-101/pr101.fs
@@ -1,14 +1,23 @@
-." here we are inside pr101.fs" cr
-144 111 99 66 22 $3f .s ."  17 Mar 01:32 UTC" cr
-." non-locally modded." cr
+\ ." here we are inside pr101.fs" cr
+
+211 188 77 33 11 $3b .s
+
+."  17 Mar 01:42 UTC" cr
+
+\ ." non-locally modded." cr
 ." words:  bwf - basic workflow" cr cr
-: bwf ." Basic workflow:" cr
-      ." 1. Edit on host PC ; gforth ./thisfile.fs" cr
-      ."    to test" cr
-      ." 2. git commit ; git push" cr
-      ." 3. arm: filemgr: erase/rename existing file" cr
-      ." 4. arm: FastHub: download revised .fs" cr
-      ." 5. arm: gforth:  test" cr ;
+
+: bwf cr cr ( -- )
+
+    ." Basic workflow:" cr cr
+
+    ."   1. pc:  edit pr101.fs ; gforth ./pr101.fs" cr
+    ."           to test" cr
+    ."   2. pc:  git commit ; git push" cr
+    ."   3. arm: filemgr: erase/rename existing file" cr
+    ."   4. arm: FastHub: download revised .fs" cr
+    ."   5. arm: gforth:  test" cr ;
+
 0 [IF]
 \ comment
 [THEN]
